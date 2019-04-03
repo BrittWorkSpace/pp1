2d41559f74ef8b54dcf98685a17838128328fc23
Michael Britt <mbritt3@greenriver.mail.edu>
2019-04-03 17:22:41 -0400

    Added function to seperate file from index.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function printArr($data)
+{
+    foreach ($data as $value)
+    {
+        echo $value;
+    }
+}
+$numArray = array(7,9,8,9,8,8,6);
+?>
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-function printArr($data)
-{
-    foreach ($data as $value)
-    {
-        echo $value;
-    }
-}
-$numArray = array(7,9,8,9,8,8,6);
-?>
 <!doctype html>
 <html lang="en">
 <head>
