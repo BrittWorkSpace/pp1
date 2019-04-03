d61eaf8bcf409811d775d76991f195d7df0864b0
Michael Britt <mbritt3@greenriver.mail.edu>
2019-04-03 17:19:28 -0400

    Added function to print an arrays contentes

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+function printArr($data)
+{
+    foreach ($data as $value)
+    {
+        echo $value;
+    }
+}
+$numArray = array(7,9,8,9,8,8,6);
+?>
 <!doctype html>
 <html lang="en">
 <head>
